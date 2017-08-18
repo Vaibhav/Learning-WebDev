2b9102c9c2ae9b687fe631bcb9ec5538c0ee37c4
Vaibhav <ishuvaibhav@gmail.com>
2017-08-18 13:34:23 +0530

    comments working

--- Rails/webblog/app/controllers/comments_controller.rb
+++ Rails/webblog/app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @post.comments.create! comments_params
+    redirect_to @post
+  end
+
+  private
+    def set_post
+        @post = Post.find(params[:post_id])
+    end
+
+    def comments_params
+        params.required(:comment).permit(:body)
+    end
 end
